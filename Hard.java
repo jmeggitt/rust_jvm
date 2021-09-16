ead39d6d518d554d2ab5af0ea875985ca99c8ca9
Jasper Meggitt <jasper.meggitt@gmail.com>
2021-09-16 11:54:20 -0400

    Add harder test case

---
+++ Hard.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Hard {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test Title");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+        System.out.println("Its working!");
+    }
+
+}
